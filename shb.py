6e633188794409a16087ac1c55f29d5b0c793c12
akshat-soni_navi <akshat.soni@navi.com>
2025-09-02 14:37:51 +0530

    Add functionality to play sound from command

--- shb.py
+++ shb.py
@@ -1,9 +1,32 @@
 import argparse
 from sounds import add_sound, list_sounds, play_sound, remove_sound
-from configure import add_configuration
+from configure import add_configuration, list_configurations, remove_configuration
 import exception_handler
 import sys
 
+def play_sound_on_cli(command):
+    if os.path.exists("config.json") and os.path.getsize("config.json") > 0:
+        try:
+            with open("config.json", "r") as f:
+                config = json.load(f)
+        except json.JSONDecodeError:
+            print("Warning: config.json is corrupted. Creating a new configuration.")
+            config = {}
+    else:
+        config = {}
+    
+    if not config:
+        print("No configurations found")
+        return
+    
+    if len(command.split()) > 2:
+        words = command.split()[:2]
+        if " ".join(words) in config:
+            play_sound_file(config[" ".join(words)])
+        elif words[0] in config:
+            play_sound_file(config[words[0]])
+    elif command in config:
+        play_sound_file(config[command])
 
 def main():
     parser = argparse.ArgumentParser(description="Shellbeats CLI Tool")
@@ -17,6 +40,8 @@ def main():
 
     config_parser = command_subparsers.add_parser('configure', help='Command to run')
     config_parser.add_argument('-a','--add', action='store_true', help='Add a new configuration for a cli command')
+    config_parser.add_argument('-l','--list', action='store_true', help='List all configurations')
+    config_parser.add_argument('-r','--remove', action='store_true', help='Remove a configuration')
     
     
     args = parser.parse_args()
@@ -32,6 +57,10 @@ def main():
     elif args.command == 'configure':
         if args.add:
             add_configuration()
+        elif args.list:
+            list_configurations()
+        elif args.remove:
+            remove_configuration()
     else:
         parser.print_help()
 
